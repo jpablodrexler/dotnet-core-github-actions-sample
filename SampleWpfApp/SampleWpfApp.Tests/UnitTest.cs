28ea2a5781da59363d37c0d24a091cc8da38845e
Juan Pablo Drexler <jpablodrexler@outlook.com>
2020-03-18 22:41:12 -0300

    Adds sample WPF .NET Core app.

---
+++ SampleWpfApp/SampleWpfApp.Tests/UnitTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace SampleWpfApp.Tests
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
